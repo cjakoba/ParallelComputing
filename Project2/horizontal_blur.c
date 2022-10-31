2fe66dc8f9116fb4f8caa29541ec14906dbfbf77
cajakob <cajakob@icloud.com>
2022-10-31 15:47:44 -0400

    Added better documentation

--- Project2/horizontal_blur.c
+++ Project2/horizontal_blur.c
@@ -5,21 +5,26 @@
 #include <omp.h>
 
 #define BLUR_AMOUNT 50
+#define THREADS 4
 
+// Contains ppm header information from original ppm file
 struct header {
 	int height, width, maxRGB;
 };
 typedef struct header header_t;
 
+// Contains pixel RGB data
 struct pixel {
 	double red, green, blue;
 };
-
 typedef struct pixel pixel_t;
+
 
 // Divides color values by 2 and increments them by the remaining pixels color values in front of them
 // until reaching the BLUR_AMOUNT, or hitting edge of the image, whichever comes first.
 void Blur(pixel_t * pixels, int max_height, int max_width) {
+
+	#pragma omp parallel for num_threads(THREADS)
 	// From top to bottom
 	for (int height = 0; height < max_height; height++) {
 		// From left to right in a single row
@@ -27,7 +32,6 @@ void Blur(pixel_t * pixels, int max_height, int max_width) {
 
 			// Select a pixel struct by current width, then include height to get actual position in struct array
 			int pixel = width + (height * max_width) - 1;
-			int z;
 
 			// For the current pixel divide its color values by 2
 			pixels[pixel].red /= 2;
@@ -35,14 +39,18 @@ void Blur(pixel_t * pixels, int max_height, int max_width) {
 			pixels[pixel].blue /= 2;
 
 			// set the remaining pixels to include in blur to BLUR_AMOUNT, or number less than BLUR_AMOUNT
-			double r = max_width - width < BLUR_AMOUNT ? max_width - width : BLUR_AMOUNT;
+			double pixels_right; 
+			if (max_width - width < BLUR_AMOUNT) {
+				pixels_right = max_width - width;
+			} else {
+				pixels_right = BLUR_AMOUNT;
+			}
 
 			// factor in the weight of the remaining pixels
-			// This is when the problem occurs when I consider edge pixels rather then just full outright using BLUR_AMOUNT
-			for (z = 1; z <= r; z++) {
-				pixels[pixel].red += pixels[pixel+z].red * (0.5 / r);
-				pixels[pixel].green += pixels[pixel+z].green * (0.5 / r);
-				pixels[pixel].blue += pixels[pixel+z].blue * (0.5 / r);
+			for (int i = 1; i <= pixels_right; i++) {
+				pixels[pixel].red += pixels[pixel + i].red * (0.5 / pixels_right);
+				pixels[pixel].green += pixels[pixel + i].green * (0.5 / pixels_right);
+				pixels[pixel].blue += pixels[pixel + i].blue * (0.5 / pixels_right);
 			}
 		}
 	}
@@ -50,6 +58,7 @@ void Blur(pixel_t * pixels, int max_height, int max_width) {
 
 
 int main(int argc, char **argv) {
+	// Timing structure
 	struct timeval current;
 
 	// Check to see if user specified two command line arguments
@@ -58,20 +67,22 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
+	// Set command line arguments to input and output file names
 	char *input_name = argv[1];
 	char *output_name = argv[2];
 	FILE *fp;
 
+	// Starts load file timing
 	gettimeofday(&current, NULL);
 	unsigned long ms_start = (current.tv_sec * 1000) + (current.tv_usec / 1000);
 
-	// Check to see if the input file exists in directory
+	// Checks if the input file exists in directory
 	if ((fp = fopen(input_name, "r")) == NULL) {
 		fprintf(stderr, "Error: %s does not exist in directory.\n", input_name);
 		exit(EXIT_FAILURE);
 	}
 
-		header_t head;
+	header_t head;
 	char str[3];
 	int	count = 3;
 	
@@ -90,7 +101,10 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
+	// Creates a 1D array of pixel_t structs based on P3 specifications of width and height
 	pixel_t * pixels = malloc(head.width * head.height * sizeof(pixel_t));
+
+	// input into each pixel_t struct array the red, green, and blue values for each pixel
 	int i = 0;
 	while (fscanf(fp, "%lf %lf %lf", &pixels[i].red, &pixels[i].green, &pixels[i].blue) == 3) {
 		i++;
@@ -98,33 +112,43 @@ int main(int argc, char **argv) {
 
 	fclose(fp);
 
+	// Stops load file timing and calculates total time
 	gettimeofday(&current, NULL);
 	unsigned long ms_end = (current.tv_sec * 1000) + (current.tv_usec / 1000);
 	printf("File loading took %ld ms.\n", ms_end - ms_start);
-
-
-	// printing heade information to a new ppm file
-	fp = fopen(output_name, "w");
-	fprintf(fp, "P3\n%d %d\n%d\n", head.width, head.height, head.maxRGB);
 	
+	// Starts blur timing
 	gettimeofday(&current, NULL);
 	ms_start = (current.tv_sec * 1000) + (current.tv_usec / 1000);
+
+	// Blurs each pixel's color values in the pixel_t struct array
 	Blur(pixels, head.height, head.width);
+	
+	// Stop blur timing and calculates total time
 	gettimeofday(&current, NULL);
 	ms_end = (current.tv_sec * 1000) + (current.tv_usec / 1000);
 	printf("Blur function took %ld ms.\n", ms_end - ms_start);
 
+	// Starts save file timing
 	gettimeofday(&current, NULL);
 	ms_start = (current.tv_sec * 1000) + (current.tv_usec / 1000);
+	
+	// printing header information to a new ppm file
+	fp = fopen(output_name, "w");
+	fprintf(fp, "P3\n%d %d\n%d\n", head.width, head.height, head.maxRGB);
 	
 	// Print to the blur .ppm file the new pixel values
 	for (i = 0; i < head.width * head.height; i++) {
 		fprintf(fp, "%d %d %d\t", (int) pixels[i].red, (int) pixels[i].green, (int) pixels[i].blue); 
+		// Print 10 pixel's worth of RGB values per line
 		if (i % 10 == 0) {
 			fprintf(fp, "\n");
 		}
 	}
 
+	fclose(fp);
+
+	// Stops save file timing and calculates total time
 	gettimeofday(&current, NULL);
 	ms_end = (current.tv_sec * 1000) + (current.tv_usec / 1000);
 	printf("File saving took %ld ms.\n", ms_end - ms_start);
