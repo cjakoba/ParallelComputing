c94e55a1ce3e80a3e31dd915c85072daa3441e51
cajakob <cajakob@icloud.com>
2022-10-31 14:21:12 -0400

    Additional command line argument and .ppm file error handling

--- Project2/horizontal_blur.c
+++ Project2/horizontal_blur.c
@@ -18,30 +18,40 @@ typedef struct pixel pixel_t;
 
 int main(int argc, char **argv) {
 
-	// Command line argument for filename to open and blur
-	char *filename = argv[1];
+	// Check to see if user specified two command line arguments
+	if(argv[1] == NULL || argv[2] == NULL) {
+		fprintf(stderr, "Specify command line arguments as ./a.out [input ppm file] [output ppm file].\n");
+		exit(EXIT_FAILURE);
+	}
+
+	char *input_name = argv[1];
+	char *output_name = argv[2];
+	FILE *fp;
+
+	// Check to see if the input file exists in directory
+	if ((fp = fopen(input_name, "r")) == NULL) {
+		fprintf(stderr, "Error: %s does not exist in directory.\n", input_name);
+		exit(EXIT_FAILURE);
+	}
 
 	header_t head;
-
-	FILE *fp;
-	fp = fopen(filename, "r");
 	char str[3];
 	int	count = 3;
 	
+	// Exit when there is no magic number matching P3
 	if (strcmp(fgets(str, 3, fp), "P3") != 0)  {
-		printf("File corrupted. Missing P3 in header. Exiting.\n");
+		fprintf(stderr, "File corrupted. Missing P3 in header. Exiting.\n");
 		exit(EXIT_FAILURE);
 	}
 
 	fscanf(fp, "%d %d", &head.width, &head.height);
 	fscanf(fp, "%d", &head.maxRGB);
 
+	// Exit when MAX RGB is not 255.
 	if (head.maxRGB != 255)  {
 		printf("File corrupted. MAX RGB value is not 255. Exiting.\n");
 		exit(EXIT_FAILURE);
 	}
-
-	printf("header file : height %d, width %d, maxrgb %d\n", head.height, head.width, head.maxRGB);
 
 	pixel_t * pixels = malloc(head.width * head.height * sizeof(pixel_t));
 	int i = 0;
@@ -52,7 +62,7 @@ int main(int argc, char **argv) {
 	fclose(fp);
 
 	// printing heade information to a new ppm file
-	fp = fopen("_blur.ppm", "w");
+	fp = fopen(output_name, "w");
 	fprintf(fp, "P3\n%d %d\n%d\n", head.width, head.height, head.maxRGB);
 
 	// From top to bottom
@@ -84,7 +94,10 @@ int main(int argc, char **argv) {
 
 	// Print to the blur .ppm file the new pixel values
 	for (i = 0; i < head.width * head.height; i++) {
-		fprintf(fp, "%d %d %d ", (int) pixels[i].red, (int) pixels[i].green, (int) pixels[i].blue); 
+		fprintf(fp, "%d %d %d\t", (int) pixels[i].red, (int) pixels[i].green, (int) pixels[i].blue); 
+		if (i % 10 == 0) {
+			fprintf(fp, "\n");
+		}
 	}
 
 
